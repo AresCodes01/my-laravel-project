431d1482a9870926e6a7290381be8155863fd710
AresCodes01 <wares.ahmad01@gmail.com>
2025-10-01 16:09:34 +0200

    added new things

--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,28 @@ use App\Models\Job;
 
 
 Route::get('/', function () {
-      return view('home');
+    return view('home');
 });
 
+Route::get('/jobs', function () {
+    $jobs = Job::with("employer")->simplePaginate(4);
 
-Route::get('/jobs', function ()  {
-        $jobs = Job::with("employer")->simplePaginate(4);
 
-
-    return view('jobs', [
+    return view('jobs/index', [
         'jobs' => $jobs
     ]);
 });
 
-
-Route::get('/jobs/{id}', function ($id)  {
-    $job = Job::find($id);
-    return view("job", ["job" => $job]);
+Route::get("/jobs/create", function () {
+    return view('jobs.create');
 });
 
 
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+
+    return view("jobs.show", ["job" => $job]);
+});
 Route::get('/contact', function () {
     return view('contact');
 });
