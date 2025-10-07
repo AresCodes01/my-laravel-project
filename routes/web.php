f8dfaa99b43d568eeff49a39195ff56c5041248d
AresCodes01 <wares.ahmad01@gmail.com>
2025-10-07 11:59:34 +0200

    Register added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -9,3 +10,5 @@ Route::view('/contact', 'contact');
 
 
 Route::resource('jobs', JobController::class);
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
