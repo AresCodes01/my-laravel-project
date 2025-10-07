6f02943940a5ee0aa1c97d08c6a65c3e03fe5c2a
AresCodes01 <wares.ahmad01@gmail.com>
2025-10-07 13:23:28 +0200

    Register Login added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -12,3 +13,7 @@ Route::view('/contact', 'contact');
 Route::resource('jobs', JobController::class);
 
 Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get("/login", [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
